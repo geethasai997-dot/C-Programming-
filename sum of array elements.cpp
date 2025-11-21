9c54bd0a280843368ae451b88a513cda9200a106
geethasai997-dot <geethasai997@gmail.com>
2025-11-21 23:21:04 +0530

    Add files via upload

---
+++ sum of array elements.cpp
@@ -0,0 +1,17 @@
+//to display sum of array elemts
+#include<stdio.h>
+int main()
+{
+	int a[5],i,sum=0;
+	printf("enter array elements");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		sum=sum+a[i];
+	}
+	printf("sum of array elements is %d\n",sum);
+	return 0;
+}
